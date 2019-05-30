54406be32b4654fbd36a8b1f2bc1404a892ada0b
Eileen <eileen.tallman@gmail.com>
2019-05-30 12:54:03 -0400

    Ready for review

--- dotm_search.py
+++ dotm_search.py
@@ -1,47 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Given a directory path, search all files in the path for a given text string
-within the 'word/document.xml' section of a MSWord .dotm file.
-
-Write a python program named `dotm_search.py`.  
-Your program should accept two cmdline arguments: First argument is the text to search for. 
-The second argument is an OPTIONAL directory of .dotm files to scan.  If this argument is omitted,
-the default path to search is the current directory.  NOTE that the optional directory `--dir [dotm-path]` actually includes two arguments (the option flag, and its value) but we are counting them as one combined argument key-value pair.
-```
-python dotm_search.py --dir ./dotm_files "$" 
-python dotm_search.py "other text"
-```
-
-- Your program should print the full path name of each file that was found to contain the search text.  If the file contains multiple matches, just count it as a single match.
-- For context, print a partial line of the dotm text that was found to contain the search text.  Limit the printed line to +/- 40 characters on each side of the matched text.  Example: `"...alculated on a per article basis (up to $500 each), the total false marking penal..."`
-- Count the total number of file matches as well as total number of files searched, and display the results before exiting.
-"""
-import my_module as mm
 import sys
+import os
+import zipfile
+import argparse
 
-# courses = ['History', 'Math', 'Physics', 'CompSci']
-# pricing_info = mm.find_pricing(courses, 'Math')
-# print(pricing_info)
+# iterate through dotm files/managing files
+def manage_file(filename, search_term):
+    """Function conducts an outer and inner zip to search the folder for .dotm files and then searches the text for matches."""
+    # Outer Zip
+    if not zipfile.is_zipfile(filename):
+        print("Error: not a zipfile.")
+        return 0
+    with zipfile.ZipFile(filename) as zip:
+        print(zip.namelist())
+    #Inner Zip#
+        with zip.open('word/document.xml') as doc:
+            # print("Searching directory " + namespace.folder)
+            for line in doc:
+                index = str(line).find(search_term)
+                if index >= 0:
+                    print("Match found in file {}".format(filename))
+                    print(" ..." + line[index-40:index+40] + "...")
+                    return True
+            return False
 
-print(sys.path)
-# import zip 
-# get documentation
-# extract xml
+
+# Inspection function
+def file_parser():
+    """Function parses the folders and files."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", help="Dotm folder to search.")
+    parser.add_argument("text", help="Text to search.")
+    return parser
+
+
 #  """Function prints test results to output file."""
-
-#     with open('input.txt', "r+") as rf:
-#         with open('output.txt', 'w') as wf:    
-#             for line in rf:
-#                 wf.write(nested_brackets(line)+'\n')
-
-__author__ = "etallman"
+__author__ = "Eileen with help from instructor"
 
 
 def main():
-    # raise NotImplementedError("Your awesome code begins here!")
-    print('dotm_search')
-    
+    files_searched = 0
+    matches = 0
+    parser = file_parser()
+    namespace = parser.parse_args()
+    for f in os.listdir(namespace.folder):
+        if not f.endswith(".dotm"):
+            print("Not a .dotm -- skipping file {}".format(f))
+            continue
+        files_searched +=1
+        full_path = os.path.join(namespace.folder, f)
+        if manage_file(full_path, namespace.text):
+            matches +=1
+    print("Total files searched: {}".format(files_searched))
+    print("Total matches found: {}".format(matches))
+
 if __name__ == '__main__':
     main()
